3f446221da6061f1b1485446768bee5eccd52071
22-3600 <189080709+22-3600@users.noreply.github.com>
2025-09-18 08:49:20 +0800

    Update index.php

--- black_basket/pages/settings/index.php
+++ black_basket/pages/settings/index.php
@@ -9,6 +9,7 @@ session_start();
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Inventory Management System</title>
     <link rel="stylesheet" href="../../assets/css/style.css">
+    <link rel="stylesheet" href="style.css">
     <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css" rel="stylesheet">
     <link rel="icon" type="image/x-icon" href="../../assets/images/icon.webp">
 </head>
@@ -405,7 +406,7 @@ session_start();
                         </div>
 
                         <div id="inventory-tab" class="settings-tab">
-                            <!-- Inventory settings content removed -->
+                            
                         </div>
 
                         <div id="advanced-tab" class="settings-tab">
@@ -416,5 +417,6 @@ session_start();
             </div>
         </div>
     </div>
+    <script src="settings.js"></script>
 </body>
 </html>
